736a0c1579105146313f91f830f84a6872c3ec04
Egor <EgorrKosolapoff@ya.ru>
2023-07-10 15:19:18 +0700

    1 задача

--- Program.cs
+++ Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿/*Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+Например:
+78 -> 8
+12-> 2
+85 -> 8
+*/
+
+
+Console.Clear();
+
+int f(int n)
+{
+    int n1 = n / 10;
+    int n2 = n % 10;
+    if (n1 > n2)
+        return n1;
+    return n2;
+}
+
+int n = new Random().Next(10, 100);
+Console.WriteLine(n);
+Console.WriteLine(f(n));
